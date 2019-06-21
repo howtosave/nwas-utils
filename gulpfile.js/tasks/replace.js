da305aa9b59374ae038316537f338b9c6d77be32
peterk <tiglek+u16office@gmail.com>
2019-06-21 21:42:37 +0900

    adding replace

---
+++ gulpfile.js/tasks/replace.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable import/no-dynamic-require */
+
+const path = require('path');
+const replacer = require('replace-in-file');
+const d = require('debug')('replace');
+
+const parseArgs = require('../utils/parse_args');
+
+const cwd = process.cwd(); // console working directory
+
+const task = async (done) => {
+  // TODO
+};
+
+module.exports = task;
